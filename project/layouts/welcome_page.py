2071d44637a1615310480c99e654d7c417db6a49
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-05-28 13:16:49 -0400

    partial update

--- project/layouts/welcome_page.py
+++ project/layouts/welcome_page.py
@@ -12,16 +12,36 @@ def welcome_page():
         html.Div([
             html.Div([
                 html.Div([
-                    html.H1("Visualize Your Finances"),
-                    html.P("Gain control over your expenses. Discover clear, visual insights into your spending to help you budget smarter and save more. - Yuh"),
+                    html.H1("Visualize Your Finances", className='welcome-slogan'),
+                    html.P("Gain control over your expenses. Discover clear, visual insights into your spending to help you budget smarter and save more.", className='welcome-paragraph'),
                     dcc.Link("Start Today", href='/sign_up', className='sign-up-btn')                          
                 ], className='welcome-text'),
-
-                html.Div([
-                    html.Img(src= '/assets/Solid_white.png')
-                ], className='welcome-image')
+                html.Img(src= '/assets/wallet.png', className='welcome-image')
             ], className= "welcome-content")
-        ], className='welcome-content-background')
-            
+        ], className='welcome-content-background'),
+
+        html.Div([
+            html.Div([
+                html.H2("About The Creators"),
+                html.P("After becoming groupmates in "),
+                html.Div([
+                    html.Div([
+                        html.Img(src='assets/masa.png', className='creator-image'),
+                        html.H3('Nagamasa (Masa) Kagami'),
+                        html.P('Electrical Engineering Student @ McGill University'),
+                        dcc.Link('LinkedIn', href='your_linkedin_profile', className='social-link'),
+                        html.P('\n'),
+                        dcc.Link('Personal Website', href='masakagami.com', className='social-link')
+                    ], className='creator-profile'),
+                    html.Div([
+                        html.Img(src='/path_to_partner_image.jpg', className='creator-image'),
+                        html.H3('Partner Name'),
+                        html.P('A brief bio or description of your partner’s background, expertise, and role in the project.'),
+                        dcc.Link('LinkedIn', href='partner_linkedin_profile', className='social-link')
+                    ], className='creator-profile')
+                ], className='creators-container')
+            ], className='welcome-about')            
+        ], className='welcome-about-background')
+
 
     ],className='welcome')
