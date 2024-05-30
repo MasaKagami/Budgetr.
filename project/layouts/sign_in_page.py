e786e4459e42ce62dea7d61bc04fa5e790f0edfa
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-05-30 11:25:04 -0400

    partial

--- project/layouts/sign_in_page.py
+++ project/layouts/sign_in_page.py
@@ -5,58 +5,37 @@ def sign_in_page():
                 html.Div([
                         html.Div([
                                 dcc.Link(
-                                html.H1("Budgetr.", className='welcome-logo'),
-                                href='/',
-                                className='logo-link'
+                                        html.H1("Budgetr.", className='welcome-logo'),
+                                        href='/',
+                                        className='logo-link'
                                 ),
                                 html.Div([
-                                dcc.Link("Sign In", href='/sign-in', className='sign-in-btn', id='tab-signin'),
-                                dcc.Link("Create Your Account", href='/sign-up', className='sign-up-btn', id='tab-signup')
-                                ], className='navbar-tabs')
+                                        dcc.Link("Sign In", href='/sign-in', className='sign-in-btn', id='tab-signin'),
+                                        dcc.Link("Create Your Account", href='/sign-up', className='sign-up-btn', id='tab-signup')
+                                        ], className='navbar-tabs')
                         ], className= 'welcome-nav')
-                        ], className= 'welcome-nav-background'),
-
-        # html.Section(className='container', children=[
-        #     html.Div(className='login-container', children=[
-        #         html.Div(className='circle circle-one'),
-        #         html.Div(className='form-container', children=[
-        #             html.Img(src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png", className='illustration', alt="illustration"),
-        #             html.H1("LOGIN", className='opacity'),
-
-
-
-        #             html.Div(children=[
-        #                 dcc.Input(type='text', placeholder='USERNAME'),
-        #                 dcc.Input(type='password', placeholder='PASSWORD'),
-        #                 html.Button('SUBMIT', className='opacity')
-        #             ], style={'display': 'flex', 'flexDirection': 'column'}),
-        #             html.Div(className='register-forget opacity', children=[
-        #                 dcc.Link('REGISTER', href=''),
-        #                 dcc.Link('FORGOT PASSWORD', href='')
-        #             ])
-
-
-
-        #         ]),
-        #         html.Div(className='circle circle-two')
-        #     ]),
-        #     html.Div(className='theme-btn-container')
-        # ])
+                ], className= 'welcome-nav-background'),
         
                 html.Div([
+                        # html.Div(className='circle-one'),
                         html.Div([
-                                html.H1("Sign In"),
                                 html.Div([
-                                dcc.Input(type='text', placeholder='USERNAME'),
-                                dcc.Input(type='password', placeholder='PASSWORD'),
-                                html.Button('SUBMIT', className='opacity')
-                                ], className='sign-in-top'),
+                                        html.Img(src="/assets/login-illustration.png", className='sign-in-illustration', alt='illustration'),
+                                        html.H1("Sign In", className='sign-in-title'),
+                                        html.Div([
+                                        dcc.Input(type='text', placeholder='USERNAME'),
+                                        dcc.Input(type='password', placeholder='PASSWORD'),
+                                        html.Button('SUBMIT', className='opacity')
+                                        ], className='sign-in-top'),
 
-                                html.Div([
-                                dcc.Link('sign up', href='/sign-up'),
-                                dcc.Link('forgot password', href='')
-                                ], className='sign-in-bottom')
+                                        html.Div([
+                                        dcc.Link('sign up', href='/sign-up'),
+                                        dcc.Link('forgot password', href='')
+                                        ], className='sign-in-bottom')
+                                ], className='sign-in')
 
-                        ], className='sign-in')
-                ], className='sign-in-form')
+                        ], className='sign-in-form'),
+                        # html.Div(className='circle-two')
+                ], className='sign-in-container'),
+        
         ], className='sign-in-page')
