aa9c5ea98bbab70f92b23ba93023fdfd531bd90b
shyamdesai03 <shyam.desai@mail.mcgill.ca>
2024-06-04 10:58:23 +0400

    ALways hide sidebar unless logged in, and cleaned up logging structure

--- project/myfinanceplanner.py
+++ project/myfinanceplanner.py
@@ -1,8 +1,7 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, State
 from flask import Flask, session
 from datetime import timedelta
 from os import urandom
-import logging
 
 # Layouts
 from layouts.dashboard_page import dashboard_page
@@ -18,7 +17,7 @@ from callbacks.spendings_callback import spendings_callback
 from callbacks.sidebar_callback import sidebar_callback
 from callbacks.authentication_callback import authentication_callback
 from callbacks.settings_callback import settings_callback
-from load_data import load_remote_database, load_local_database, print_dataframes
+from load_data import load_database, print_dataframes, setup_logging
 
 # Initialize Flask server
 server = Flask(__name__)
@@ -28,38 +27,24 @@ server.secret_key = urandom(24) # Generate a secret key for the session
 server.config['SESSION_PERMANENT'] = True  # Make sessions permanent
 server.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)  # Set session lifetime
 
-# Set up logging
-# logging.basicConfig(level=logging.DEBUG)
-
-# @server.before_request
-# def before_request():
-#     logging.debug(f"Session data before request: {dict(session)}")
-
-# @server.after_request
-# def after_request(response):
-#     logging.debug(f"Session data after request: {dict(session)}")
-#     return response
-
 # Initialize Dash app
 app = Dash(__name__, server=server, suppress_callback_exceptions=True) # Suppress callback exceptions ensures callbacks not initially in the app layout are not raised as errors
 app.title = 'Budgetr.'
 
-# Use remote database or local database for user authentication and input forms
-USE_REMOTE_DB = True
+LOGGING = True # Logging for database records and user sessions
+USE_REMOTE_DB = True # Pick databse for user authentication and spending records
 
-# Load data from the database
-if USE_REMOTE_DB:
-    transactions_df, categories_df, users_df, monthly_budgets_df, categorical_budgets_df = load_remote_database()
-else:
-    transactions_df, categories_df, users_df, monthly_budgets_df, categorical_budgets_df = load_local_database()
-# print_dataframes(transactions_df, categories_df, users_df, monthly_budgets_df, categorical_budgets_df)
+setup_logging(server, LOGGING)
+
+transactions_df, categories_df, users_df, monthly_budgets_df, categorical_budgets_df = load_database(USE_REMOTE_DB)
+# print_dataframes(LOGGING, USE_REMOTE_DB)
 print("USERS DB\n", users_df[:5])
 
 # ------------------------------------------------------------------------------
 # Main App layout with Sidebar
 
 app.layout = html.Div([
-    dcc.Store(id='local-store', storage_type='session'),  # 'session' storage type allows data to persist across pages and until browser is closed
+    dcc.Store(id='session', storage_type='session'),  # 'session' storage type allows data to persist across pages and until browser is closed
     
     html.Link(
         href='https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap',
@@ -82,7 +67,7 @@ app.layout = html.Div([
         dcc.Link('Settings', href='/settings', className='tab', id='tab-settings'),
         dcc.Link('Support', href='/support', className='tab', id='tab-support'),
         dcc.Link('Logout', href='/logout', className='tab', id='tab-logout')
-    ], className='sidebar', id='sidebar'),
+    ], style={'display': 'none'}, className='sidebar', id='sidebar',),
 
     html.Div(id='page-content') # Placeholder to display the page content
 
@@ -92,39 +77,48 @@ app.layout = html.Div([
 # Callback to toggle between pages from the sidebar
 @app.callback(
     Output('page-content', 'children'),
-    [Input('url', 'pathname'),
-     Input('local-store', 'data')]
+    [Input('url', 'pathname')],
+    [State('session', 'data')]
 )
 
 def display_page(pathname, session_data):
+    print("Session data: ", session_data)
+
     if pathname == '/':
         return welcome_page()
     
     elif pathname == '/sign-in':
-        return sign_in_page()
+        if session and session['logged_in']:
+            return dcc.Location(href='/dashboard', id='redirect')
+        else:
+            return sign_in_page()
     
     elif pathname == '/sign-up':
-        return sign_up_page()
+        if session and session['logged_in']:
+            return dcc.Location(href='/dashboard', id='redirect')
+        else:
+            return sign_up_page()
     
     elif pathname == '/dashboard':
-        if session_data and session_data.get('logged_in'):
+        if session and session['logged_in']:
             return dashboard_page(transactions_df)
         else:
             return dcc.Location(href='/sign-in', id='redirect')
         
     elif pathname == '/record':
-        if session_data and session_data.get('logged_in'):
+        if session and session['logged_in']:
             return spendings_page(categories_df, categorical_budgets_df)
         else:
             return dcc.Location(href='/sign-in', id='redirect')
         
     elif pathname == '/settings':
-        if session_data and session_data.get('logged_in'):
+        if session and session['logged_in']:
             return settings_page()
         else:
             return dcc.Location(href='/sign-in', id='redirect')
         
     elif pathname == '/logout':
+        session.clear()
         print("User logged out")
         return dcc.Location(href='/', id='redirect') # Redirect to the welcome page after logging out
 
@@ -139,10 +133,10 @@ def display_page(pathname, session_data):
     [Input('url', 'pathname')]
 )
 def toggle_sidebar_visibility(pathname):
-    if pathname in ['/', '/sign-in', '/sign-up']:
-        return {'display': 'none'}  # Hide sidebar on welcome, sign-in, and sign-up pages
+    if session and session['logged_in'] and pathname in ['/dashboard', '/record', '/settings', '/support']:
+        return {'display': 'flex'}  # Show sidebar in the app when logged in
     else:
-        return {'display': 'flex'}  # Show sidebar as a flexbox on all other pages
+        return {'display': 'none'}  # Hide sidebar when not logged in
 
 # ------------------------------------------------------------------------------
 # Callbacks for every page
