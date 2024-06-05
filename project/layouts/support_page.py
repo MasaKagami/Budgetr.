ee809214ebf12d38c20d9074c606b23055bba6b5
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-06-05 14:27:37 -0400

    yapyap

--- project/layouts/support_page.py
+++ project/layouts/support_page.py
@@ -2,25 +2,30 @@ from dash import html, dcc
 
 def support_page():
     return html.Div([
-        html.H1('Contact Form'),
         html.Div([
-            dcc.Input(
-                id='name-input',
-                type='text',
-                placeholder='your name'
-            ),
-            dcc.Input(
-                id='email-input',
-                type='email',
-                placeholder='your email'
-            ),
-        ], className='support-user'),
-        html.Textarea(
-            id='message-input',
-            placeholder='Enter your message here...',
-        ),
-        html.Button('SEND', id='send-button', n_clicks=0),
-        html.Div(id='support-status')
-        
-
-    ], className='support-page')
+            html.H1('Contact Form'),
+            html.Div([    
+                html.Div([
+                    html.H2('Name'),
+                    dcc.Input(
+                        id='name-input',
+                        type='text',
+                        placeholder='your name'
+                    ),
+                    html.H2('Email'),
+                    dcc.Input(
+                        id='email-input',
+                        type='email',
+                        placeholder='your email'
+                    ),
+                ], className='support-user'),
+                html.H2('Message to Creator'),
+                html.Textarea(
+                    id='message-input',
+                    placeholder='Enter your message here...',
+                ),
+                html.Button('SEND', id='send-button', n_clicks=0),
+                html.Div(id='support-status')
+            ], className='support-page-container')
+        ], className='support-page')
+    ], className='support')
